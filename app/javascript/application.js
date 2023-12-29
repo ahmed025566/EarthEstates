e28706d30400ebb61ad8e6bfaa0ce2b1397e350d
ahmed025566 <ahmedsalah025566#gmail.com>
2023-12-30 00:26:55 +0200

    Fix errors

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,14 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-
-$(() => {
-   $('.propPhoto').on('click', () => {
-    console.log('photo is clicked')
-   })
-})
-
-
-$(() => {
-    console.log('App is up and running with JQuery')
-})
